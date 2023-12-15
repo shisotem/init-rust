c9acdd12ba0cd8f5aae57615288f6e9ea65b3ff8
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-15 20:08:51 +0900

    feat(guessing-game): compare guess with secret-number

--- book/guessing_game/src/main.rs
+++ book/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -16,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
