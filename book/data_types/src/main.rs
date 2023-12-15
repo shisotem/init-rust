ffe0d46bf30e2b4d07bd97c1f013d889a6f4e80f
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-15 23:56:33 +0900

    feat(data-types): division

--- book/data_types/src/main.rs
+++ book/data_types/src/main.rs
@@ -1,15 +1,6 @@
 fn main() {
-    // numeric literal
-    println!("98_222: {}", 98_222);
-    println!("0xff: {}", 0xff);
-    println!("0o77: {}", 0o77);
-    println!("0b1111_0000: {}", 0b1111_0000);
-    println!("b'A': {}", b'A');
-    println!("b'B': {}", b'B');
-    // 98_222: 98222
-    // 0xff: 255
-    // 0o77: 63
-    // 0b1111_0000: 240
-    // b'A': 65
-    // b'B': 66
+    let quotient: f64 = 56.7 / 32.2;
+    println!("{}", quotient); // 1.7608695652173911
+    let floored: i32 = 2 / 3;
+    println!("{}", floored); // 0
 }
