68d57e6a1d7d9f6da0ca1a66c2531c7aa78f2ebd
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-16 00:23:31 +0900

    feat(data-types): array

--- book/data_types/src/main.rs
+++ book/data_types/src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-
-    let five_hundred = tup.0;
-    let six_point_four = tup.1;
-    let one = tup.2;
-
-    let (x, y, z) = tup;
+    // Array
+    // - stack memory
+    // - fixed-length
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    let first = arr[0];
+    let second = arr[1];
+    let all_three = [3; 5];
 }
