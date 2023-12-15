830719ac914b249033b5cf3f3c65aac8abb08420
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-15 23:42:00 +0900

    feat(data-types): numeric literal

---
+++ book/data_types/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // numeric literal
+    println!("98_222: {}", 98_222);
+    println!("0xff: {}", 0xff);
+    println!("0o77: {}", 0o77);
+    println!("0b1111_0000: {}", 0b1111_0000);
+    println!("b'A': {}", b'A');
+    println!("b'B': {}", b'B');
+    // 98_222: 98222
+    // 0xff: 255
+    // 0o77: 63
+    // 0b1111_0000: 240
+    // b'A': 65
+    // b'B': 66
+}
