6509438b6b02ef7119fbde7c008373e52d661301
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-15 22:01:19 +0900

    feat(variables): const

--- book/variables/src/main.rs
+++ book/variables/src/main.rs
@@ -1,6 +1,10 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    println!("MAX_POINTS: {}", MAX_POINTS);
 }
