56e6f12f741cd713f6ca9f7140a49593df18da39
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-16 00:45:16 +0900

    feat(data-types): index-out-of-bounds panic

--- book/data_types/src/main.rs
+++ book/data_types/src/main.rs
@@ -1,23 +1,33 @@
+use std::io;
+
 fn main() {
-    // Array
-    // - stack memory
-    // - fixed-length
-    let months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
+    // panic: runtime error
+
     let arr: [i32; 5] = [1, 2, 3, 4, 5];
-    let first = arr[0];
-    let second = arr[1];
-    let all_three = [3; 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = arr[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+
+    // Please enter an array index.
+    // 5
+    // thread 'main' panicked at src/main.rs:19:19:
+    // index out of bounds: the len is 5 but the index is 5
+    // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
 }
