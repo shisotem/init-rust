ef85d3ddb145bb8a16bf59fa6aa3e5ea499f27cc
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-16 20:27:49 +0900

    refactor(input): refactor get_num function to handle error cases and improve UX

--- book/fibonacci/src/main.rs
+++ book/fibonacci/src/main.rs
@@ -1,14 +1,29 @@
 use std::io;
 
 fn main() {
-    let num = get_num();
-    println!("{num}");
+    let num;
+    loop {
+        match get_num() {
+            Ok(n) => {
+                num = n;
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                continue;
+            }
+        };
+    }
+
 }
 
-fn get_num() -> i32 {
+fn get_num() -> Result<i32, String> {
     let mut num = String::new();
-    io::stdin()
-        .read_line(&mut num)
-        .expect("Fail to read user input");
-    num.trim().parse().expect("Not a number")
+    match io::stdin().read_line(&mut num) {
+        Ok(_) => match num.trim().parse::<i32>() {
+            Ok(n) => Ok(n),
+            Err(_) => Err(String::from("Input was not a number")),
+        },
+        Err(_) => Err(String::from("Failed to read user input")),
+    }
 }
