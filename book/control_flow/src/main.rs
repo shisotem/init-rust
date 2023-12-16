484a97214e60617c83b1e726096e0ca7e2625fd8
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-16 11:33:17 +0900

    feat(control-flow): if statement requires a boolean

---
+++ book/control_flow/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // // error
+    // let number = 3;
+    // if number {
+    //     println!("number was three");
+    // }
+
+    let number = 3;
+    if number != 0 {
+        println!("number was something other than zero"); // arm
+    } else {
+        println!("number was zero"); // arm
+    }
+}
