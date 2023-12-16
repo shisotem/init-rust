34d93382c1d27e5e9cf5b250583c9b31aee954b1
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-16 15:32:40 +0900

    feat(calc): convert temperature and show it

--- book/temperature_converter/src/main.rs
+++ book/temperature_converter/src/main.rs
@@ -20,6 +20,13 @@ fn main() {
         };
 
         let temperature = get_temperature();
+        let converted_temperature = match selection {
+            OPTION_C_TO_F => celsius_to_fahrenheit(temperature),
+            OPTION_F_TO_C => fahrenheit_to_celsius(temperature),
+            _ => unreachable!(),
+        };
+
+        println!("Converted temperature: {}", converted_temperature);
         break;
     }
 }
@@ -41,3 +48,10 @@ fn get_temperature() -> f64 {
     temperature.trim().parse().expect("Invalid temperature")
 }
 
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    celsius * 9.0 / 5.0 + 32.0
+}
+
+fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+    (fahrenheit - 32.0) * 5.0 / 9.0
+}
