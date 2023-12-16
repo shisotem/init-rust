f862211a6113baca0b2f369fc3bfe5435de39149
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-16 22:00:50 +0900

    feat(display): enable to display lyrics

--- book/twelve_days_of_xmas/src/main.rs
+++ book/twelve_days_of_xmas/src/main.rs
@@ -1,8 +1,33 @@
 fn main() {
+    let ordinal = [
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
+        "tenth", "eleventh", "twelfth",
+    ];
+    let characters = [
+        "a partridge in a pear tree.",
+        "two turtle doves,",
+        "three French hens,",
+        "four calling birds,",
+        "five golden rings,",
+        "six geese a-laying,",
+        "seven swans a-swimming,",
+        "eight maids a-milking,",
+        "nine ladies dancing,",
+        "ten lords a-leaping,",
+        "eleven pipers piping,",
+        "twelve drummers drumming,",
+    ];
+
     for day in 1..=12 {
-        println!("On a {} day Christmas my true love sent to me", day);
+        println!(
+            "On a {} day Christmas my true love sent to me",
+            ordinal[day - 1]
+        );
         for num in (1..=day).rev() {
-            println!("{}", num);
+            if day != 1 && num == 1 {
+                print!("and ")
+            }
+            println!("{}", characters[num - 1]);
         }
         println!();
     }
