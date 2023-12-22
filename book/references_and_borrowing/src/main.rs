cbf70f16c7b4b48be139f2645de5555789d41e14
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-23 00:07:26 +0900

    feat(references_and_borrowing): &mut String

--- book/references_and_borrowing/src/main.rs
+++ book/references_and_borrowing/src/main.rs
@@ -4,9 +4,18 @@ fn main() {
         let len = calculated_length(&s1);
         println!("The length of '{}' is {}.", s1, len); // [3]: s1(stack) -> "hello"(heap)
     } // s1 scope out, drop s1 and "hello"
+
+    {
+        let mut s = String::from("hello");
+        change(&mut s);
+    }
 }
 
 fn calculated_length(s: &String) -> usize {
     // [2]: s(stack) -> s1(stack) -> "hello"(heap)
     s.len()
 } // s scope out, but not drop s1 and "hello"
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
