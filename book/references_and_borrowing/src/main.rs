cd2e262316edc71cb76c24475b0b892f99535359
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-22 23:59:20 +0900

    feat(references_and_borrowing): borrowing

---
+++ book/references_and_borrowing/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    {
+        let s1 = String::from("hello"); // [1]: s1(stack) -> "hello"(heap)
+        let len = calculated_length(&s1);
+        println!("The length of '{}' is {}.", s1, len); // [3]: s1(stack) -> "hello"(heap)
+    } // s1 scope out, drop s1 and "hello"
+}
+
+fn calculated_length(s: &String) -> usize {
+    // [2]: s(stack) -> s1(stack) -> "hello"(heap)
+    s.len()
+} // s scope out, but not drop s1 and "hello"
