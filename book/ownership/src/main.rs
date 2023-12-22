1a64bb43f2b24cfca749b4b2d5c9f27e04d82758
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-22 22:22:52 +0900

    feat(ownership): ownership and function args

--- book/ownership/src/main.rs
+++ book/ownership/src/main.rs
@@ -26,4 +26,21 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
     println!("s1 = {}, s2 = {}", s1, s2); // ok
+
+    // ---
+
+    makes_copy(x); // copy
+    takes_ownership(s1); // move
+
+    // x: available, s1: unavailable
+
+    // ---
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
 }
