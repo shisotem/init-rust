00bc161b5da0a820aa1fddd170f38a207dcd9e6e
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-22 21:45:15 +0900

    feat(ownership): move

--- book/ownership/src/main.rs
+++ book/ownership/src/main.rs
@@ -6,4 +6,15 @@ fn main() {
         let mut _s_heap: String = String::from("hello");
         _s_heap.push_str(", world!");
     } // drop _s_heap
+
+    // ---
+
+    // -> Stack (x: 5, y: 5)
+    let x = 5;
+    let y = x;
+
+    // Move
+    let s1 = String::from("hello");
+    let s2 = s1;
+    // println!("{}, world!", s1); // error
 }
