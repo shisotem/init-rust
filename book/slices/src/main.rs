715e076975072906d6b2159c79bb190435006feb
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-25 13:24:56 +0900

    feat(slices): make the API more general using &str parameter

--- book/slices/src/main.rs
+++ book/slices/src/main.rs
@@ -22,6 +22,14 @@ fn main() {
         // s.clear(); // error!
         // println!("The first word in \"{}\" is {}", s, word);
     }
+
+    {
+        let my_string = String::from("hello world");
+        let word = awesome_first_word(&my_string[..]);
+        let my_string_literal = "hello world";
+        let word = awesome_first_word(&my_string_literal[..]);
+        let word = awesome_first_word(my_string_literal);
+    }
 }
 
 // problem: s and length are not linked
@@ -39,6 +47,18 @@ fn length_of_first_word(s: &String) -> usize {
 }
 
 fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn awesome_first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
