926f5e870c6d336ce6aad77cf67aaa9b7554067c
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-25 20:20:43 +0900

    feat(rectangles): add can_hold method

--- book/rectangles/src/main.rs
+++ book/rectangles/src/main.rs
@@ -8,6 +8,10 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
@@ -51,6 +55,24 @@ fn main() {
             "The area of the rectangle is {} square pixels.",
             rect1.area()
         );
+    }
+
+    {
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+        let rect2 = Rectangle {
+            width: 10,
+            height: 40,
+        };
+        let rect3 = Rectangle {
+            width: 60,
+            height: 45,
+        };
+
+        println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+        println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     }
 }
 
