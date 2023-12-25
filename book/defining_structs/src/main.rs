8496136f1038a712c9c46f868fde1954e24522fb
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-25 15:43:13 +0900

    feat(defining_structs): structure updating notation

--- book/defining_structs/src/main.rs
+++ book/defining_structs/src/main.rs
@@ -18,6 +18,27 @@ fn main() {
     {
         let user1 = build_user(String::from("foo@example.com"), String::from("foo"));
     }
+
+    {
+        let user1 = User {
+            email: String::from("someone@example.com"),
+            username: String::from("someusername123"),
+            active: true,
+            sign_in_count: 1,
+        };
+
+        // let user2 = User {
+        //     email: String::from("another@example.com"),
+        //     username: String::from("anotherusername567"),
+        //     active: user1.active,
+        //     sign_in_count: user1.sign_in_count,
+        // };
+        let user2 = User {
+            email: String::from("another@example.com"),
+            username: String::from("anotherusername567"),
+            ..user1
+        };
+    }
 }
 
 fn build_user(email: String, username: String) -> User {
