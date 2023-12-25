1eee064a61c869b8bb8e036f6785be1d06b42f0a
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-25 15:57:21 +0900

    feat(defining_structs): tuple struct

--- book/defining_structs/src/main.rs
+++ book/defining_structs/src/main.rs
@@ -39,6 +39,14 @@ fn main() {
             ..user1
         };
     }
+
+    {
+        struct Color(i32, i32, i32);
+        struct Point(i32, i32, i32);
+
+        let black = Color(0, 0, 0);
+        let origin = Point(0, 0, 0);
+    }
 }
 
 fn build_user(email: String, username: String) -> User {
