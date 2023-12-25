63a67aca087948a9a119ddb7684f37edffcdc501
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-25 18:21:18 +0900

    feat(rectangles): the meaning of the calc is unclear

--- book/rectangles/src/main.rs
+++ book/rectangles/src/main.rs
@@ -1,14 +1,26 @@
 fn main() {
     {
-        let width1 = 30;
-        let height1 = 50;
+        // let width1 = 30;
+        // let height1 = 50;
+        // println!(
+        //     "The area of the rectangle is {} square pixels.",
+        //     area(width1, height1)
+        // );
+    }
+
+    {
+        let rect1 = (30, 50);
         println!(
             "The area of the rectangle is {} square pixels.",
-            area(width1, height1)
-        )
+            area(rect1)
+        );
     }
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
