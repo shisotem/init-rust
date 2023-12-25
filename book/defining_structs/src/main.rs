363af7f493ee3edb4b9545bb11b9409007c434bf
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-25 15:21:15 +0900

    feat(defining_structs): define struct and make instance

---
+++ book/defining_structs/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    {
+        struct User {
+            username: String,
+            email: String,
+            sign_in_count: u64,
+            active: bool,
+        }
+        let mut user1 = User {
+            email: String::from("someone@example.com"),
+            username: String::from("someusername123"),
+            active: true,
+            sign_in_count: 1,
+        };
+        user1.email = String::from("anotheremail@example.com");
+    }
+}
