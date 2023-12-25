1434711c3cd177dbee7a71a8829db050af693a0c
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-25 23:32:41 +0900

    feat(defining_enums): variant with data

--- book/defining_enums/src/main.rs
+++ book/defining_enums/src/main.rs
@@ -3,10 +3,20 @@ enum IpAddrKind {
     V6,
 }
 
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
 fn main() {
     {
         let four = IpAddrKind::V4;
         route(four);
+    }
+
+    {
+        let home = IpAddr::V4(127, 0, 0, 1);
+        let loopback = IpAddr::V6(String::from("::1"));
     }
 }
 
