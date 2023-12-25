6abd109258aa07fdbdf9c9aac5e8bb02f6445b76
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-26 00:16:11 +0900

    feat(defining_enums): flexibility of enums

--- book/defining_enums/src/main.rs
+++ book/defining_enums/src/main.rs
@@ -11,15 +11,27 @@ enum IpAddr {
 // struct Ipv4Addr {
 //     //
 // }
-
 // struct Ipv6Addr {
 //     //
 // }
-
 // enum IpAddr {
 //     V4(Ipv4Addr),
 //     V6(Ipv6Addr),
 // }
+
+// struct QuitMessage; // unit struct
+// struct MoveMessage { // struct
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String); // tuple struct
+// struct ChangeColorMessage(i32, i32, i32); // tuple struct
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
 fn main() {
     {
@@ -30,6 +42,13 @@ fn main() {
     {
         let home = IpAddr::V4(127, 0, 0, 1);
         let loopback = IpAddr::V6(String::from("::1"));
+    }
+
+    {
+        let quit_message = Message::Quit;
+        let move_message = Message::Move { x: 1, y: 2 };
+        let write_message = Message::Write(String::from("hello"));
+        let change_color_message = Message::ChangeColor(1, 2, 3);
     }
 }
 
