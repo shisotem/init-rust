28a0eec55dfc56e67ceec40203314149273c1f53
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-25 15:28:37 +0900

    feat(defining_structs): structure as expression

--- book/defining_structs/src/main.rs
+++ book/defining_structs/src/main.rs
@@ -1,11 +1,11 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
 fn main() {
     {
-        struct User {
-            username: String,
-            email: String,
-            sign_in_count: u64,
-            active: bool,
-        }
         let mut user1 = User {
             email: String::from("someone@example.com"),
             username: String::from("someusername123"),
@@ -13,5 +13,18 @@ fn main() {
             sign_in_count: 1,
         };
         user1.email = String::from("anotheremail@example.com");
+    }
+
+    {
+        let user1 = build_user(String::from("foo@example.com"), String::from("foo"));
+    }
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
     }
 }
