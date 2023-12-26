f32576cf07a4d0fa754889c36380a58965d1ce02
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-26 21:47:06 +0900

    feat(match_if_let): _ placeholder

--- book/match_if_let/src/main.rs
+++ book/match_if_let/src/main.rs
@@ -26,6 +26,17 @@ fn main() {
         let six = plus_one(five);
         let none = plus_one(None);
     }
+
+    {
+        let some_u8_value = 0u8;
+        match some_u8_value {
+            1 => println!("one"),
+            3 => println!("three"),
+            5 => println!("five"),
+            7 => println!("seven"),
+            _ => (), // unit value: nothing will happen
+        }
+    }
 }
 
 // fn value_in_cents(coin: Coin) -> u32 {
