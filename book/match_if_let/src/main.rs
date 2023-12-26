461764b5f7a9befa3d8eca9d458d496c2744e340
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-26 20:39:00 +0900

    feat(match_if_let): match

---
+++ book/match_if_let/src/main.rs
@@ -0,0 +1,17 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn main() {}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
