8b113aae3c1c615eb31b89fef5b5963db16f0985
shisotem <129283841+shisotem@users.noreply.github.com>
2023-12-26 21:11:03 +0900

    feat(match_if_let): binding the value of a variant to a variable

--- book/match_if_let/src/main.rs
+++ book/match_if_let/src/main.rs
@@ -1,20 +1,35 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
-fn main() {}
+fn main() {
+    {
+        let coin = Coin::Quarter(UsState::Alaska);
+        let value = value_in_cents(coin);
+        println!("value: {}", value);
+        // State quarter from Alaska!
+        // value: 25
+    }
+}
 
 fn value_in_cents(coin: Coin) -> u32 {
     match coin {
-        Coin::Penny => {
-            println!("Lucky Penny!");
-            1
-        }
+        Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
